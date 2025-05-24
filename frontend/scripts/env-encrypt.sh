4222edde2669e3000c014a132dd573d2f92a7854
Matyas Angyal <matyas.angyal@jaystack.com>
2025-05-24 21:11:08 +0200

    feat: add basic env config

---
+++ frontend/scripts/env-encrypt.sh
@@ -0,0 +1,5 @@
+dotenvx encrypt --env-file .env.0_test
+dotenvx encrypt --env-file .env.1_local
+dotenvx encrypt --env-file .env.2_development
+dotenvx encrypt --env-file .env.3_staging
+dotenvx encrypt --env-file .env.4_production
