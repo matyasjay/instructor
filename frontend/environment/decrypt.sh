f98d2e2e0439a88aeacafb2c13892dc0c1189c5e
Matyas Angyal <matyas.angyal@jaystack.com>
2025-05-24 21:58:13 +0200

    feat: move pnpm toplevel and add husky

--- frontend/environment/decrypt.sh
+++ frontend/environment/decrypt.sh
@@ -1,5 +1,6 @@
-dotenvx decrypt --env-file environment/0_test
-dotenvx decrypt --env-file environment/1_local
-dotenvx decrypt --env-file environment/2_development
-dotenvx decrypt --env-file environment/3_staging
-dotenvx decrypt --env-file environment/4_production
+pwd
+dotenvx decrypt --env-file frontend/environment/0_test
+dotenvx decrypt --env-file frontend/environment/1_local
+dotenvx decrypt --env-file frontend/environment/2_development
+dotenvx decrypt --env-file frontend/environment/3_staging
+dotenvx decrypt --env-file frontend/environment/4_production
