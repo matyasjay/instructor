18cb2b17a1b4909f5b97f30f6c44d0af21336694
Matyas Angyal <matyas.angyal@jaystack.com>
2025-05-24 21:27:33 +0200

    chore: move frontend to own module

---
+++ frontend/environment/decrypt.sh
@@ -0,0 +1,5 @@
+dotenvx decrypt --env-file environment/0_test
+dotenvx decrypt --env-file environment/1_local
+dotenvx decrypt --env-file environment/2_development
+dotenvx decrypt --env-file environment/3_staging
+dotenvx decrypt --env-file environment/4_production
