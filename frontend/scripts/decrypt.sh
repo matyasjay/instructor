9f0f3ea4520424fec9b0464b7333b6eb01e69e6d
Matyas Angyal <matyas.angyal@jaystack.com>
2025-05-25 15:04:57 +0200

    chore: add port env

--- frontend/scripts/decrypt.sh
+++ frontend/scripts/decrypt.sh
@@ -1,6 +1,6 @@
 pwd
-dotenvx decrypt --env-file frontend/environment/0_test
-dotenvx decrypt --env-file frontend/environment/1_local
-dotenvx decrypt --env-file frontend/environment/2_development
-dotenvx decrypt --env-file frontend/environment/3_staging
-dotenvx decrypt --env-file frontend/environment/4_production
+dotenvx decrypt --env-file .env.test
+dotenvx decrypt --env-file .env.local
+dotenvx decrypt --env-file .env.development
+dotenvx decrypt --env-file .env.staging
+dotenvx decrypt --env-file .env.production
