d653ff548ce998715c55229c06f8b1a90fcc0cd9
Matyas Angyal <matyas.angyal@jaystack.com>
2025-05-25 19:20:01 +0200

    chore(ci): refactor bump cli script

--- cli/bump.sh
+++ cli/bump.sh
@@ -1,1 +1,10 @@
-node cli/semver.mjs && sh cli/tag.sh
+node cli/semver.mjs 
+
+VERSION=$(node -p "require('./package.json').version")
+
+git tag -a "v$VERSION" -m "Release v$VERSION"
+
+git add package.json
+git commit -m "chore(ci): bump version v$VERSION"
+
+git push origin "v$VERSION"
--- cli/tag.sh
+++
@@ -1,4 +0,0 @@
-VERSION=$(node -p "require('./package.json').version")
-git tag -a "v$VERSION" -m "Release v$VERSION"
-git push origin "v$VERSION"
-
