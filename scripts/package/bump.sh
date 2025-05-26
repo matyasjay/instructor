fe18972d711a1b7a41e72f7e3777c38b6aef9f5e
Matyas Angyal <matyas.angyal@jaystack.com>
2025-05-26 21:51:22 +0200

    chore: check bump script

--- scripts/package/bump.sh
+++ scripts/package/bump.sh
@@ -6,9 +6,9 @@ C_SEAGREEN2="\033[38;5;83m"
 C_GREY46="\033[38;5;243m"
 C_GREY62="\033[38;5;247m"
 
-echo "${F_BOLD}${F_DIM}Update package versions...${NO_FORMAT}"
-echo -n "${F_DIM}${C_GREY46}Project: '$PROJECT'${NO_FORMAT}"
-echo -n "${F_DIM}${C_GREY46}Cluster: '$PROJECT-<local|dev|stg|prod>'${NO_FORMAT}"
+echo "${F_BOLD}${F_DIM}Update package versions.${NO_FORMAT}"
+echo "${F_DIM}${C_GREY46}Project: '$PROJECT'${NO_FORMAT}"
+echo "${F_DIM}${C_GREY46}Cluster: '$PROJECT-<local|dev|stg|prod>'${NO_FORMAT}"
 
 if ! git diff-index --quiet HEAD -- || [ -n "$(git ls-files --others --exclude-standard)" ]; then
   echo "${F_BOLD}${C_INDIANRED1}Working directory is not clean. Commit or stash your changes first.${NO_FORMAT}"
@@ -18,17 +18,17 @@ else
 fi
 
 VERSION=$(node -p "require('./package.json').version")
-echo -n "${C_INDIANRED1}Deprecated ${F_BOLD}${VERSION}${NO_FORMAT}"
+echo "${C_INDIANRED1}Deprecated ${F_BOLD}${VERSION}${NO_FORMAT}"
 
 node script/package/semver.mjs
 
 VERSION=$(node -p "require('./package.json').version")
-echo -n "${C_SEAGREEN2}Upstream ${F_BOLD}${VERSION}${NO_FORMAT}"
+echo "${C_SEAGREEN2}Upstream ${F_BOLD}${VERSION}${NO_FORMAT}"
 
 sed -i '' "s/Release-.*-blue/Release-${VERSION}-blue/" README.md
 git tag -a "v${VERSION}" -m "Release v${VERSION}"
 
-echo -n "${F_DIM}${C_GREY62}Tag '${PROJECT} v${VERSION}' ready to be released.${NO_FORMAT}."
+echo "${F_DIM}${C_GREY62}Tag '${PROJECT} v${VERSION}' ready to be released.${NO_FORMAT}."
 
 git add \
   package.json \ 
@@ -41,8 +41,8 @@ git add \
 
 git commit -m "chore(ci): bump version v${VERSION}"
 
-echo -n "${F_DIM}${C_GREY62}Changes are committed and ready to push.${NO_FORMAT}"
+echo "${F_DIM}${C_GREY62}Changes are committed and ready to push.${NO_FORMAT}"
 
-echo -n "${F_BOLD}${C_SEAGREEN2}Updated package versions successfully!${NO_FORMAT}"
+echo "${F_BOLD}${C_SEAGREEN2}Updated package versions successfully!${NO_FORMAT}"
 
 exit 0
