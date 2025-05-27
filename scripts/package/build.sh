1aeddc41891e0183324b7fdb6cd803857d6d2893
Matyas Angyal <matyas.angyal@jaystack.com>
2025-05-27 14:56:25 +0200

    chore: run fmt

--- scripts/package/build.sh
+++ scripts/package/build.sh
@@ -7,6 +7,10 @@ C_GREY46="\033[38;5;243m"
 C_GREY62="\033[38;5;247m"
 
 echo "\n${F_BOLD}Build packages${NO_FORMAT}${C_GREY46}\n"
-cd frontend && make build
-cd ../http && make build
+cd frontend 
+make fmt
+make build
+cd ../http 
+make fmt
+make build
 echo "${NO_FORMAT}"
