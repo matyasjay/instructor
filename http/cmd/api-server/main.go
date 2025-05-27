4f734be4ea23004263659554e259fd6bb135b956
Matyas Angyal <matyas.angyal@jaystack.com>
2025-05-27 14:31:05 +0200

    feat: add db connection to main http

--- http/cmd/api-server/main.go
+++ http/cmd/api-server/main.go
@@ -4,10 +4,32 @@ import (
 	"github.com/labstack/echo/v4"
 	"net/http"
 	"os"
+	"database/sql"
+  "fmt"
+
+  _ "github.com/lib/pq"
+)
+
+const (
+  host     = "localhost"
+  port     = 5432
+  user     = "postgres"
+  password = "C77ahH3co@"
+  dbname   = "postgres"
 )
 
 func main() {
 	e := echo.New()
+
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
 
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "ok")
