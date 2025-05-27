29985688f3fa915085923cc9bfc5ab62accf6148
Matyas Angyal <matyas.angyal@jaystack.com>
2025-05-27 22:24:03 +0200

    feat: add confirm before sign and push

--- scripts/git/push.sh
+++ scripts/git/push.sh
@@ -6,5 +6,20 @@ C_SEAGREEN2="\033[38;5;83m"
 C_GREY46="\033[38;5;243m"
 C_GREY62="\033[38;5;247m"
 
-make sign
-sh scripts/package/bump.sh "$(PROJECT)" && git push
+
+
+echo "\n${F_BOLD}${C_INDIANRED1}--- Hit ENTER to confirm COMMIT and PUSH task ---${NO_FORMAT}\n"
+
+read -r input
+
+if [ -z "$input" ]; then
+  echo "\n${F_BOLD}Sign and push local changes${NO_FORMAT}\n"
+  make sign
+  sh scripts/package/bump.sh "$(PROJECT)" && git push
+else
+  echo "\n${C_GREY62}Cancelled!${NO_FORMAT}\n"
+fi
+
+echo "\n${F_BOLD}Done!${NO_FORMAT}\n"
+
+
