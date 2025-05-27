b2f64d1401df4bae7c501d90a74b58806fda1a5e
Matyas Angyal <matyas.angyal@jaystack.com>
2025-05-27 21:18:04 +0200

    feat: use pnpm-workspace

---
+++ scripts/package/test.sh
@@ -0,0 +1,5 @@
+  #"test:unit": "dotenvx run ../.env.test -- vitest run",
+  #"test:unit:watch": "dotenvx run ../.env.test -- vitest watch",
+  #"test:unit:coverage": "dotenvx run ../.env.test -- vitest run --coverage"
+
+  echo "Not implemented!"
