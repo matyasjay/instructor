477008c2feefefd0b6ccffc34450e118efcc7a26
Matyas Angyal <matyas.angyal@jaystack.com>
2025-06-01 17:33:02 +0200

    chore: adjust kubectl info

--- scripts/package/info.sh
+++ scripts/package/info.sh
@@ -14,18 +14,19 @@ else
   PROJECT=$1
 fi
 
-echo "\n${F_BOLD}Cluster info for '${PROJECT}-local'${NO_FORMAT}"
+echo "\n${F_BOLD}Cluster info for '${PROJECT}'${NO_FORMAT}"
 echo "${C_GREY46}"
 echo "Change project: make bump PROJECT=my-project${NO_FORMAT}"
 echo "${F_DIM}"
 echo "Project: '$PROJECT'"
-echo "Namespace: '$PROJECT-deployment'${NO_FORMAT}"
+echo "Namespace: '$PROJECT-app'${NO_FORMAT}"
+echo "Namespace: '$PROJECT-database'${NO_FORMAT}"
 echo "\n${F_DIM}Application services:${NO_FORMAT}\n${C_GREY46}"
 
-kubectl get svc -n "${PROJECT}-deployment"
+kubectl get svc -n "${PROJECT}-app"
 
 echo "\n${NO_FORMAT}${F_DIM}Database services:${NO_FORMAT}\n${C_GREY46}"
 
-kubectl get svc -n database
+kubectl get svc -n "${PROJECT}-database"
 
 echo "${NO_FORMAT}"
