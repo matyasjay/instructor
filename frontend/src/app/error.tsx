c7c17f55a6bf457d0740ce2ee0d619cd010e5bdd
Matyas Angyal <matyas.angyal@jaystack.com>
2025-06-04 15:26:14 +0200

    chore: rework landing page content

--- frontend/src/app/error.tsx
+++ frontend/src/app/error.tsx
@@ -1,3 +1,5 @@
+import { AlertCircleIcon } from "lucide-react";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { parseErrorObject } from "@/lib/utils";
 import { ErrorBoundary as ReactErrorBoundary } from "react-error-boundary";
 import Layout from "./layout";
@@ -13,11 +15,24 @@ export function ErrorDisplay({
 
   return (
     <Layout>
-      <div>
-        <div role="alert">
-          <div className="border border-x-0 border-t-0 border-red-400 bg-red-100 px-4 py-3 text-red-700">
-            <p>{message}</p>
-          </div>
+      <div className="flex flex-col w-full gap-3.5 mx-auto min-h-10/12 px-9">
+        <div className="w-full pt-4">
+          <h1 className="flex scroll-m-20 text-lg font-semibold tracking-tight mb-4">
+Error
+          </h1>
+          <Alert variant="destructive">
+            <AlertCircleIcon />
+            <AlertTitle>
+            {message}
+            </AlertTitle>
+            <AlertDescription>
+              <ul className="list-disc list-inside text-m">
+                <li>The page may have moved or been deleted</li>
+                <li>You might have mistyped the URL</li>
+                <li>The link you followed could be outdated</li>
+              </ul>
+            </AlertDescription>
+          </Alert>
         </div>
       </div>
     </Layout>
--- frontend/src/app/landing/index.tsx
+++ frontend/src/app/landing/index.tsx
@@ -1,9 +1,43 @@
 function Landing() {
   return (
-    <div className="flex flex-col max-w-100 gap-3.5 mx-auto justify-center align-middle min-h-10/12">
-      <h1 className="scroll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance mb-9">
-        Landing
-      </h1>
+    <div className="flex flex-col gap-3.5 mx-auto justify-center align-middle min-h-full bg-gray-900">
+      <div className="relative isolate px-6 pt-14 lg:px-8">
+        <div
+          className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+          aria-hidden="true"
+        >
+          <div
+            className="relative left-[calc(50%-11rem)] aspect-1155/678 w-144.5 -translate-x-1/2 rotate-30 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-288.75"
+            style={{
+              clipPath:
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+            }}
+          ></div>
+        </div>
+        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+          <div className="text-center">
+            <h1 className="scroll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance mb-9">
+              Instructor
+            </h1>
+            <p className="mt-8 text-lg font-medium text-pretty text-primary-foreground sm:text-xl/8">
+              Above trial and error. Build your ideas with clean structure, best
+              practices, versioned logic and reusable parts for systems that scale with you.
+            </p>
+          </div>
+        </div>
+        <div
+          className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+          aria-hidden="true"
+        >
+          <div
+            className="relative left-[calc(50%+3rem)] aspect-1155/678 w-144.5 -translate-x-1/2 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-288.75"
+            style={{
+              clipPath:
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+            }}
+          ></div>
+        </div>
+      </div>
     </div>
   );
 }
