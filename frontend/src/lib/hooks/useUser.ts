6bc1afe6e78ecf78b5eade44888398f111754b9d
Matyas Angyal <matyas.angyal@jaystack.com>
2025-06-06 12:32:27 +0200

    fix: add auth dep to useUser

--- frontend/src/lib/hooks/useUser.ts
+++ frontend/src/lib/hooks/useUser.ts
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { useAuth } from "@/components/context/auth";
 import { STORAGE } from "@/config/cookies";
 
 const defaultUser: User = {
@@ -9,10 +10,11 @@ const defaultUser: User = {
 
 export default function useUser() {
   const [user, setUser] = useState<User>(defaultUser);
+  const { authenticated } = useAuth();
 
   useEffect(() => {
     setUser(JSON.parse(window.localStorage.getItem(STORAGE.USER) ?? "{}"));
-  }, []);
+  }, [authenticated]);
 
   return user;
 }
